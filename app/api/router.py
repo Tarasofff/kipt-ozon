7c2afdf442c40d4d912166a8b64709eba5a2718c
Артур Тарасов <arthur.tarasov.xv70@gmail.com>
2025-08-18 14:13:05 +0300

    add patient router

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.controllers import user_router
+from app.api.controllers import user_router, patient_router
 
 api_router = APIRouter()
 
 api_router.include_router(user_router)
+api_router.include_router(patient_router)
