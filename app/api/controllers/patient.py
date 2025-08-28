6c08ac27236223a2c93eff2a1c35ea0cfce5b467
Артур Тарасов <arthur.tarasov.xv70@gmail.com>
2025-08-28 15:49:45 +0300

    add check_patient_exists in patient controller

--- app/api/controllers/patient.py
+++ app/api/controllers/patient.py
@@ -6,6 +6,9 @@ from app.db.models import Patient
 from app.repository.patient import PatientRepository
 from app.schemas.patient import PatientCreate, PatientRead, PatientUpdate
 from typing import List
+from app.api.dependencies import (
+    check_patient_exists,
+)
 
 router = APIRouter(prefix=app_config.api_v1_prefix.patient, tags=["Patients"])
 
@@ -27,7 +30,7 @@ async def create(
 
 @router.get("/{id}", response_model=PatientRead, status_code=status.HTTP_200_OK)
 async def get_by_id(
-    id: int,
+    id: int = Depends(check_patient_exists),
     patient_repo: PatientRepository = Depends(get_patient_repository),
 ):
     return await patient_repo.get_by_id(id)
@@ -35,7 +38,7 @@ async def get_by_id(
 
 @router.get("/", response_model=List[PatientRead], status_code=status.HTTP_200_OK)
 async def get_all(
-    limit: int = Query(10, ge=1, le=1000),  # по умолчанию 10, от 1 до 1000
+    limit: int = Query(10, ge=1, le=100),  # по умолчанию 10, от 1 до 100
     offset: int = Query(0, ge=0),  # по умолчанию 0, не может быть отрицательным
     patient_repo: PatientRepository = Depends(get_patient_repository),
 ):
@@ -44,8 +47,8 @@ async def get_all(
 
 @router.put("/{id}", response_model=PatientRead, status_code=status.HTTP_200_OK)
 async def update(
-    id: int,
     patient_data: PatientUpdate,
+    id: int = Depends(check_patient_exists),
     patient_repo: PatientRepository = Depends(get_patient_repository),
 ):
     patient = patient_data.model_dump()
